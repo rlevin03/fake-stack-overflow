3a89396afb09f860034f234322236456cfafe7f6
Robert Levin <blevin0521@gmail.com>
2025-03-18 12:38:29 -0400

    updated session

--- server/models/schema/session.schema.ts
+++ server/models/schema/session.schema.ts
@@ -5,16 +5,11 @@ import { Schema } from 'mongoose';
  *
  * This schema defines the structure for storing sessions in the database.
  * Each session includes the following fields:
- * - `sessionID`: The unique identifier for the session.
  * - `versions`: The versions of the session.
  * records the timestamp of the creation and the last update.
  */
 const sessionSchema: Schema = new Schema(
   {
-    sessionID: {
-      type: String,
-      required: true,
-    },
     versions: {
       type: [String],
       default: [],
--- shared/types/session.d.ts
+++ shared/types/session.d.ts
@@ -1,9 +1,17 @@
+import { ObjectId } from 'mongodb';
+
 /**
  * A session is a collection of versions of a document that can be used to track changes over time.
- * - `sessionID`: The unique identifier for the session.
  * - `versions`: The past versions of the session.
  */
-export interface DatabaseSession {
-  sessionID: string;
+export interface Session {
   versions: string[];
+}
+
+/**
+ * Represents a session stored in the database.
+ * - `_id`: Unique identifier for the session.
+ */
+export interface DatabaseSession extends Session {
+  _id: ObjectId;
 }
